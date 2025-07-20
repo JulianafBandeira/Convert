f4a733065b94865cd7da22a4a5726f794108a85e
JulianafBandeira <julianafbbarbosa@gmail.com>
2025-07-20 18:56:23 -0300

    formatando moeda

--- scripts.js
+++ scripts.js
@@ -1,42 +1,48 @@
-const USD = 5.58
-const EUR = 6.49
-const GBP = 7.48
+const USD = 5.58;
+const EUR = 6.49;
+const GBP = 7.48;
 
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
-const footer = document.querySelector("main footer")
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
 
 amount.addEventListener("input", () => {
-  
   const hasCharacters = /\D+/g;
   amount.value = amount.value.replace(hasCharacters, "");
 });
 
-
 form.onsubmit = (e) => {
   e.preventDefault();
 
-  switch(currency.value){
+  switch (currency.value) {
     case "USD":
-        convertCurrency(amount.value, USD, "US$")
-        break
+      convertCurrency(amount.value, USD, "US$");
+      break;
     case "EUR":
-        convertCurrency(amount.value, EUR, "€")
-        break    
+      convertCurrency(amount.value, EUR, "€");
+      break;
     case "GBP":
-        convertCurrency(amount.value, GBP, "£")
-        break    
-    }
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
 };
 
-function convertCurrency(amount, price, symbol){
-    try {
-      footer.classList.add("show-result")   
-    } catch (error) {
-        console.log(error)
-        footer.classList.remove("show-result")
-        alert("Não foi possível converter a moeda")
-    }
+function convertCurrency(amount, price, symbol) {
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+    footer.classList.add("show-result");
+  } catch (error) {
+    console.log(error);
+    footer.classList.remove("show-result");
+    alert("Não foi possível converter a moeda");
+  }
 }
 
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRl",
+  });
+}
